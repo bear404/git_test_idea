8dc1565562e6659ac0139bb26ca85fc411ac7c42
FXH <fxh@qq.com>
2019-12-28 21:23:30 +0800

    增加了一个函数showmessage 并在主函数中调用

--- git_idea_test/src/com/mingbyte/git_test/app.java
+++ git_idea_test/src/com/mingbyte/git_test/app.java
@@ -4,6 +4,11 @@ package com.mingbyte.git_test;
 public class app {
     public static void main(String[] args) {
         System.out.println("hello git project");
+        showmessage();
+    }
+
+    public static void showmessage(){
+        System.out.println("this is func showmessage");
     }
 
 }
