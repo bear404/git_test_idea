1c00572dc71adb9a3e25b952083bd5f1c66820a8
FXH <fxh@qq.com>
2019-12-28 21:46:10 +0800

    在主函数中删除了对方法的调用

--- git_idea_test/src/com/mingbyte/git_test/app.java
+++ git_idea_test/src/com/mingbyte/git_test/app.java
@@ -4,7 +4,6 @@ package com.mingbyte.git_test;
 public class app {
     public static void main(String[] args) {
         System.out.println("hello git project");
-        showmessage();
     }
 
     public static void showmessage(){
