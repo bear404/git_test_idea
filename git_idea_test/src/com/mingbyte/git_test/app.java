8827c4c6dfa664b404322f829291a93997452b63
FXH <fxh@qq.com>
2019-12-28 22:38:01 +0800

    在master分支的主函数中添加一个输出

--- git_idea_test/src/com/mingbyte/git_test/app.java
+++ git_idea_test/src/com/mingbyte/git_test/app.java
@@ -5,6 +5,7 @@ public class app {
     public static void main(String[] args) {
         System.out.println("hello git project");
         branch1_app.show();
+        System.out.println("this is main");
     }
 
     public static void showmessage(){
